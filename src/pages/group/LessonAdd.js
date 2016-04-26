9b719c15896a4aa9a316915aa78d2b855b1c89b4
dayatz <dayat.py@gmail.com>
2016-04-26 22:47:39 +0800

    add lessonAdd page

--- src/pages/group/LessonAdd.js
+++ src/pages/group/LessonAdd.js
@@ -1,9 +1,32 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import FlatButton from 'material-ui/lib/flat-button'
 
 class LessonAdd extends React.Component {
-    render () {
-
+    componentWillMount() {
+        if (!this.context.group.is_owner) {
+            this.context.router.replace(`/groups/${this.context.groupId}/lessons`)
+        }
     }
+    backButton() {
+        this.context.router.replace(`/groups/${this.context.groupId}/lessons`)
+    }
+    render () {
+        return (
+            <div>
+                <div>
+                    <FlatButton label='< Back'
+                        onClick={this.backButton.bind(this)} />
+                </div>
+                LessonAdd
+            </div>
+        )
+    }
+}
+
+LessonAdd.contextTypes = {
+    router: React.PropTypes.object,
+    groupId: React.PropTypes.string,
+    group: React.PropTypes.object
 }
 
 export default LessonAdd;
