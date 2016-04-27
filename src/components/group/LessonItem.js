0467eed7b23143a05bb7630f24b5a6890382b27a
dayatz <dayat.py@gmail.com>
2016-04-27 19:55:24 +0800

    show lesson items list

--- src/components/group/LessonItem.js
+++ src/components/group/LessonItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Paper from 'material-ui/lib/paper'
 import FlatButton from 'material-ui/lib/flat-button'
 
 const LessonItem = (props, context) => {
@@ -11,10 +12,20 @@ const LessonItem = (props, context) => {
                 }} />
         )
     }
+
+    var renderItemList = props.lesson.files.map(file => {
+        return <FlatButton target='_blank' linkButton={true} label={file.name} href={file.file} />
+    });
     return (
-        <div>
-        {props.lesson.title}
-        {renderDeleteButton}
+        <div style={{width: 300, borderRadius: 10,
+                    border: '1px solid #eee',
+                    marginBottom: 10, padding: 10,
+                    float: 'left', marginLeft: 10}}>
+            {props.lesson.title}
+            <div>
+                {renderItemList}
+            </div>
+            {renderDeleteButton}
         </div>
     )
 }
